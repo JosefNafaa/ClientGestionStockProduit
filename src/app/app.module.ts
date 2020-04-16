23891917fefecfddc62ded0f091ab9567a1b3ecd
Yousef Nafaa <nafaa.youssef93@gmail.com>
2020-04-16 20:46:54 +0200

    ajout de module de routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProduitComponent } from './produit/produit.component';
 import {ProduitMockService} from './produit/produit.mock.service';
 import {ContentComponent} from './content/content.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import {SidebarComponent} from './sidebar/sidebar.component';
     ProduitComponent,
     ContentComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    DashboardComponent
 
 
 
