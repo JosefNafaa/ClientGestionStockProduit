433b368e4485dd30e254ee5a2469ebdd7e631767
Yousef Nafaa <nafaa.youssef93@gmail.com>
2020-04-17 04:17:13 +0200

    crud client 3eme patie

--- src/app/produit/produit.mock.service.ts
+++ src/app/produit/produit.mock.service.ts
@@ -8,9 +8,9 @@ export class ProduitMockService{
  PRODUITS: Produit[] = [];
 
 constructor() {
-  let p1: Produit =new Produit('Livre',15,10);
-  let p2: Produit =new Produit('Souris',20,5);
-  let p3: Produit =new Produit('Clavier',10,7);
+  let p1: Produit =new Produit(1,'Livre',15,10);
+  let p2: Produit =new Produit(2,'Souris',20,5);
+  let p3: Produit =new Produit(3,'Clavier',10,7);
 
   this.PRODUITS.push(p1);
   this.PRODUITS.push(p2);
